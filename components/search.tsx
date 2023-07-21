97f26daeecb2bb373218a1a1a36bee1f3c6e6b5f
Vincent Brown <admin@gmasterplanner.com>
2023-07-20 20:26:16 -0400

    temp checkin

--- components/search.tsx
+++ components/search.tsx
@@ -5,14 +5,18 @@ import  { RestApi } from '../api/search';
 import { Utils } from '../api/utils';
 import { useRouter } from 'next/navigation'
 
+//var template = require('../templates/search.html');
+
 export default function Search() {
     const[query, setQuery] = React.useState('');
     const router = useRouter()
     let api = new RestApi();
     let utils = new Utils();
+    //console.log(template);
 
     async function getResults(){
         console.log(query);
+        
         let response = await api.doSearch(query);
         let results: string [] = [];
         response.SearchModel.Results.forEach((r: any) => {
